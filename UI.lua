74eca76957cae3c2ee916dce6ee4d8317e369bf2
Nathan PintoAguila <nathan.pintoaguila.gamedesign@gmail.com>
2025-08-22 09:14:35 +0200

    Correction emplacement UI Player1

--- UI.lua
+++ UI.lua
@@ -1,5 +1,5 @@
 local UI = {
-    player1ScorePosition = {x = 40, y = 40},
+    player1ScorePosition = {x = 83, y = 40},
     player2ScorePosition = {x = 600, y = 40},
     winText1 = "Player 1 Wins !",
     winText2 = "Player 2 Wins !"
